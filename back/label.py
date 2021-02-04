d959aa1a7064bf206bd93971ff5711963322c066
manfred.madelaine <manfred.madelaine@outlook.fr>
2021-02-04 23:13:17 +0100

    :rocket: Tag payments

--- back/label.py
+++ back/label.py
@@ -6,18 +6,6 @@ import database_label as l_db
 DB_NAME = "database/accounting.db"
 
 
-"""
-    create_label(name, token) 
-    
-    match(tokens, paymnent)
-        [matched tokens]
-
-    ? get_all_tokens(label_name) 
-    
-    saveMapping(pymt id, token id)
-"""
-
-
 # ------------------- Label -------------------
 
 
@@ -181,7 +169,6 @@ def save_tagged_payment(tagged_payments):
 
     [l_db.insert_tagged_payment(con, *tp.to_tuple()) for tp in tagged_payments]
 
-
     raw_payments = l_db.select_all_tagged_payments(con)
     # l_db.db.print_table(raw_payments)
 
@@ -199,6 +186,32 @@ def test():
     create(new_label)
     new_label = "Employeur SG"
     create(new_label)
+    new_label = "Nexity Studea"
+    create(new_label)
+    new_label = "Quantum Cats Group"
+    create(new_label)
+    new_label = "Auchan Villebon"
+    create(new_label)
+    new_label = "Carrefour Market"
+    create(new_label)
+    new_label = "Lessive"
+    create(new_label)
+    new_label = "RATP"
+    create(new_label)
+    new_label = "Navigo"
+    create(new_label)
+    new_label = "Bouygues Telecom"
+    create(new_label)
+    new_label = "Electricite de France"
+    create(new_label)
+    new_label = "DGFIP impots"
+    create(new_label)
+    new_label = "PayPal"
+    create(new_label)
+    new_label = "Netflix"
+    create(new_label)
+    new_label = "Maison Chapin"
+    create(new_label)
 
     # update name
     # update_name(6, "Caisse d'Allocation Familliale")
@@ -213,11 +226,38 @@ def test():
     add_token(6, new_token)
     new_token = "SOCIETE GENERALE MOTIF: Appointements"
     add_token(7, new_token)
+    new_token = "Nexity studea"
+    add_token(8, new_token)
+    new_token = "Quantum Cats Group"
+    add_token(9, new_token)
+    new_token = "AUCHAN VILLEBON"
+    add_token(10, new_token)
+    new_token = "CARREFOURMARKET"
+    add_token(11, new_token)
+    new_token = "JLT MATIC"
+    add_token(12, new_token)
+    new_token = "RATP"
+    add_token(13, new_token)
+    new_token = "Navigo"
+    add_token(14, new_token)
+    new_token = "BOUYGUES TELECOM"
+    add_token(15, new_token)
+    new_token = "ELECTRICITE DE FRANCE"
+    add_token(16, new_token)
+    new_token = "DGFIP IMPOT"
+    add_token(17, new_token)
+    new_token = "PAYPAL"
+    add_token(18, new_token)
+    new_token = "Netflix"
+    add_token(19, new_token)
+    new_token = "MAISON CHOPIN"
+    add_token(20, new_token)
 
 
 def test_on_payments():
     # get pymts
     import payment as p
+
     payments = p.get_all_payments()
     tag(payments)
 
